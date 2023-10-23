2076ef7bf4382ad140d7244f9df4368218eda83a
Alberto Santos Dias <82539821+alberto-d1as@users.noreply.github.com>
2023-10-23 17:20:28 -0300

    Update file_transfer_bkp.sh

--- file_transfer_bkp.sh
+++ file_transfer_bkp.sh
@@ -3,19 +3,14 @@
 
 # Diretório dos backups a serem migrados
 local_dir="/data/backups"
-
 # Servidor físico
 remote_server="user@ip"
-
 # Diretório do Servidor físico
 remote_dir="/home/user/bkp"
 
-# Loop para transferir todos os arquivos de bkp
+# Loop para transferir os arquivo
 for file in "$local_dir"/DB_BAK_*.tar.gz; do
-   # Extrai o nome do arquivo sem o caminho
-   filename=$(basename "$file")
-
-    #copiar os arquivos
+    filename=$(basename "$file")    
     scp $remote_server/$filename $remote_dir/
 
 done
